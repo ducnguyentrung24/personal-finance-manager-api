cc14e8dac6bd27501b737f37d3f6bdea40ee5e89
ducnguyentrung24 <2251061745@e.tlu.edu.vn>
2026-03-11 20:34:25 +0700

    fix: enable CORS for frontend domain

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ const reportRoutes = require('./modules/report/report.route');
 const app = express();
 
 // Middleware
-app.use(cors({*}));
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
